746a92a8535a4a256f1a383198a1cfe715ee04f3
dabedoyaLasa <51960004+dabedoyaLasa@users.noreply.github.com>
2023-02-10 15:45:20 -0500

    Se crea componente login.

--- src/App.js
+++ src/App.js
@@ -1,19 +1,22 @@
 import './App.css';
 import { useEffect } from 'react';
+import Login from './Components/Login';
 function App() {
 
   useEffect(()=>{
    const consultarAPI = async ()=>{
-      const url = "http://takeunitsofmeasurementservice.somee.com/Jobs/GetJobs";
-      const respuesta =await fetch(url);
-
-      console.log(respuesta);
+    const url = "https://localhost:44346/api/GetJobs";
+    const respuesta = await fetch(url)
+    const resultado = await respuesta.json();
+    console.log(resultado);
+   
    }
+
    consultarAPI();
   },[]);//El arreglo vacio es para que se ejecute una vez 
   return (
     <div>
-      <h1>Actualizo</h1>
+      <Login></Login>
     </div>
   );
 }
